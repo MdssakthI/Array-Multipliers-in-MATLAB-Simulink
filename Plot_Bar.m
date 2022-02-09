9570bc3f23eb6de58c609f861f45de7a84591d92
MdssakthI <43088069+MdssakthI@users.noreply.github.com>
2022-02-09 18:05:11 +0800

    Data Analysis
    Creating a script to analyse data outcomes from Simuilink simulations

---
+++ Plot_Bar.m
@@ -0,0 +1,14 @@
+function [Data_CDM,Data_RCA,FA,Title_Plot] = Plot_Bar(Data_CDM,Data_RCA,FA,Title_Plot)
+%PLOT_BAR Summary of this function goes here
+%   Detailed explanation goes here
+b_data=[];
+for i=1:6
+   b_data=[b_data ; Data_CDM(i) Data_RCA(i)] 
+end
+figure()
+bar(b_data);
+title(Title_Plot)
+xticklabels(FA);
+legend('Carry-Down Multiplier Design','Binary-RCA Multiplier');
+end
+
