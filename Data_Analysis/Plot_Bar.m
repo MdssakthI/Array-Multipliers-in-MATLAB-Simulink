2a6a1347c91dfba1145cb8d3386cabcc42bfd889
MdssakthI <43088069+MdssakthI@users.noreply.github.com>
2022-04-10 20:26:06 +0800

    Data Analysis

--- Data_Analysis/Plot_Bar.m
+++ Data_Analysis/Plot_Bar.m
@@ -1,11 +1,13 @@
 function [A,B,Title,XLabel,LegendBar] = Plot_Bar(A,B,Title,XLabel,LegendBar)
 %PLOT_BAR Summary of this function goes here
-%   Detailed explanation goes here
-AB=[];
-for i=1:size(A,2)
-   AB=[AB ; A(i) B(i)] 
-end
-figure()
+%A and B are two data inputs
+%xlabel is for the x-axis labels. There is a bug if data exceeds 10 values
+%Title and LegendBar variables are as described
+AB=[A B];
+% for i=1:size(A,2)
+%    AB=[AB ; A(i) B(i)] %Concatenate data beside each other to make side by side comparison
+% end
+%figure()
 bar(AB);
 set(gca,'xticklabel',XLabel)
 title(Title)
